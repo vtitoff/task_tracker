4e47d9aa3095d63ad2f61f728e741e218180f92e
vtitoff <v.titoff2010@gmail.com>
2025-05-03 18:08:32 +0300

    Добавить валидацию на ключ новой очереди

--- app/tracker/models/queue.py
+++ app/tracker/models/queue.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import RegexValidator
 from django.db import models
 
 from . import TimeStampedMixin, UUIDMixin
@@ -7,7 +8,11 @@ User = get_user_model()
 
 
 class Queue(UUIDMixin, TimeStampedMixin):
-    key = models.CharField(max_length=50, unique=True)
+    key = models.CharField(
+        max_length=50,
+        unique=True,
+        validators=[RegexValidator(regex="^[A-Z]+$", message="Только заглавные латинские буквы!", code="invalid_key")],
+    )
     name = models.CharField(max_length=100)
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
