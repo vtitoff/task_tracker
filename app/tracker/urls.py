e4f041ebe22bcd0cb07698d7eea93e0a281cdbd9
vtitoff <v.titoff2010@gmail.com>
2025-05-07 00:06:17 +0300

    Поправить баг при добавлении тегов

--- app/tracker/urls.py
+++ app/tracker/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import QueuesView, QueueDetailView, TaskDetailView, AddTask, AddQueue
+from .views import QueuesView, QueueDetailView, TaskDetailView, AddTask, AddQueue, ChangeTags
 
 
 urlpatterns = [
@@ -7,5 +7,6 @@ urlpatterns = [
     path("queues/create_queue/", AddQueue.as_view(), name="create_queue"),
     path("queues/<slug:key>/", QueueDetailView.as_view(), name="queue"),
     path("queues/<slug:key>/create_task/", AddTask.as_view(), name="queue_create_task"),
+    path("tasks/<slug:task_key>/change_tags/", ChangeTags.as_view(), name="change_tags"),
     path("tasks/<slug:task_key>/", TaskDetailView.as_view(), name="task"),
 ]
--- app/tracker/views.py
+++ app/tracker/views.py
@@ -1,9 +1,9 @@
 import traceback
 
 from django.http import Http404
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, DetailView, CreateView
+from django.views.generic import TemplateView, DetailView, CreateView, View
 from .models import Queue, Task, Tag, Comment
 from .forms import (
     AddTaskForm,
@@ -86,20 +86,6 @@ class TaskDetailView(DetailView):
         form = TaskDescriptionChangeForm(request.POST, instance=task)
         if form.is_valid():
             form.save()
-
-        form = TaskTagsChangeForm(request.POST)
-        if form.is_valid():
-            tag = Tag.objects.get_or_create(name=form.cleaned_data["tag"])
-            task_key = self.kwargs.get("task_key")
-            queue_key, task_number = task_key.split("-")
-            task = get_object_or_404(
-                Task.objects.select_related("queue"), queue__key=queue_key, number_in_queue=task_number
-            )
-            if tag[0] in task.tags.all():
-                task.tags.remove(tag[0])
-            else:
-                task.tags.add(tag[0])
-
         try:
             form = TaskCommentForm(request.POST)
             if form.is_valid():
@@ -112,6 +98,42 @@ class TaskDetailView(DetailView):
         except Exception:
             logger.error(traceback.format_exc())
         return self.get(request, *args, **kwargs)
+
+
+class ChangeTags(DetailView):
+    template_name = "task_detail.html"
+    model = Task
+
+    def get_object(self):
+        task_key = self.kwargs.get("task_key")
+        try:
+            queue_key, task_number = task_key.split("-")
+        except ValueError:
+            raise Http404("Invalid task key format")
+
+        return get_object_or_404(
+            Task.objects.select_related("queue"), queue__key=queue_key, number_in_queue=task_number
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags_form"] = TaskTagsChangeForm()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = TaskTagsChangeForm(request.POST)
+        task_key = self.kwargs.get("task_key")
+        if form.is_valid():
+            tag = Tag.objects.get_or_create(name=form.cleaned_data["tag"])
+            queue_key, task_number = task_key.split("-")
+            task = get_object_or_404(
+                Task.objects.select_related("queue"), queue__key=queue_key, number_in_queue=task_number
+            )
+            if tag[0] in task.tags.all():
+                task.tags.remove(tag[0])
+            else:
+                task.tags.add(tag[0])
+        return redirect("task", task_key=task_key, permanent=True)
 
 
 class AddTask(CreateView):
